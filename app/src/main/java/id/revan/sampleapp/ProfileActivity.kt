114e08fba8f461a732eb457e2e87e33e3a350079
revando_tdc <revze@outlook.co.id>
2020-02-01 17:14:59 +0700

    Add navigation from profile to edit profile

--- app/src/main/java/id/revan/sampleapp/ProfileActivity.kt
+++ app/src/main/java/id/revan/sampleapp/ProfileActivity.kt
@@ -1,12 +1,19 @@
 package id.revan.sampleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        btn_next.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
